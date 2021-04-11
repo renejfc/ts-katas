37fd7c876f54e3c5044babff8e8dba7f5241bb46
ReneJFC <hola@renejfc.dev>
2021-04-11 02:33:28 +0200

    :white_check_mark: test: multiple of 5

--- FizzBuzz/fizzbuzz.test.ts
+++ FizzBuzz/fizzbuzz.test.ts
@@ -4,8 +4,16 @@ describe('fizzbuzz function', () => {
   it("Should return Fizz if it's multiple of 3", () => {
     const res = 'Fizz'
 
-    expect(fizzbuzz(3)).toBe('res')
-    expect(fizzbuzz(6)).toBe('res')
-    expect(fizzbuzz(12)).toBe('res')
+    expect(fizzbuzz(3)).toBe(res)
+    expect(fizzbuzz(6)).toBe(res)
+    expect(fizzbuzz(12)).toBe(res)
+  })
+
+  it("Should return Buzz if it's multiple of 5", () => {
+    const res: string = 'Buzz'
+
+    expect(fizzbuzz(5)).toBe(res)
+    expect(fizzbuzz(10)).toBe(res)
+    expect(fizzbuzz(20)).toBe(res)
   })
 })
