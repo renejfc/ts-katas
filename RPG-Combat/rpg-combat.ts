f1ec438873113ffcafa95acd421add72b92c5823
ReneJFC <hola@renejfc.dev>
2021-04-13 03:09:07 +0200

    :sparkles: feat: chars can deal damage

--- RPG-Combat/rpg-combat.ts
+++ RPG-Combat/rpg-combat.ts
@@ -2,10 +2,22 @@ interface ICharacter {
   healthPoints: number
   level: number
   isAlive: boolean
+  attack(amount: number, victim: Character): void
 }
 
 export class Character implements ICharacter {
   healthPoints: number = 1000
   level: number = 1
   isAlive: boolean = true
+
+  attack(amount: number, victim: Character): void {
+    const hp: any = victim.healthPoints
+    
+    if(hp < amount ) {
+      victim.isAlive = false
+      victim.healthPoints = 0
+    } else {
+      victim.healthPoints -= amount
+    }
+  }
 }
