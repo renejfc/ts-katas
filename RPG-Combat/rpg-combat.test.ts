25bf2f89b31f84f77c8f71397d7e44807e7dffae
ReneJFC <hola@renejfc.dev>
2021-04-13 03:00:54 +0200

    :white_check_mark: 1st iteration 2nd requeriments tests

--- RPG-Combat/rpg-combat.test.ts
+++ RPG-Combat/rpg-combat.test.ts
@@ -19,3 +19,22 @@ describe('Character created', () => {
     expect(char.isAlive).toBe(true)
   })
 })
+
+describe('Characters can deal damage to anothers Characters', () => {
+  it('Dealed damage should be subtracted from the victim health', () => {
+    const viper: Character = new Character()
+    const breach: Character = new Character()
+
+    viper.attack(300, breach)
+    expect(breach.healthPoints).toBe(700)
+  })
+
+  it('When damage exceeds the victim current Health it becomes 0 and the victim dies', () => {
+    const cypher: Character = new Character()
+    const sova: Character = new Character()
+
+    cypher.attack(1100, sova)
+    expect(sova.healthPoints).toBe(0)
+    expect(sova.isAlive).toBe(false)
+  })
+})
