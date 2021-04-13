ccaa530204884b8ade3f24e96c984f3d6cbf26a5
ReneJFC <hola@renejfc.dev>
2021-04-13 22:55:52 +0200

    :sparkles: add: heal method

--- RPG-Combat/rpg-combat.ts
+++ RPG-Combat/rpg-combat.ts
@@ -1,20 +1,22 @@
 interface ICharacter {
   healthPoints: number
+  maxHealthPoints: number
   level: number
   isAlive: boolean
   attack(amount: number, victim: Character): void
+  heal(amount: number, victim: Character): void
 }
 
 export class Character implements ICharacter {
   healthPoints: number
+  maxHealthPoints: number = 1000
   level: number
   isAlive: boolean
 
   constructor({hp, level, isAlive}: {hp?: number, level?: number, isAlive?: boolean} = {}) {
-    this.healthPoints = hp || 1000
+    this.healthPoints = (typeof hp !== 'undefined') ? hp : 1000
     this.level = level || 1
-    this.isAlive = isAlive || true
-    
+    this.isAlive = isAlive || (this.healthPoints > 0)
   }
 
   attack(amount: number, victim: Character): void {
@@ -25,6 +27,12 @@ export class Character implements ICharacter {
       victim.healthPoints = 0
     } else {
       victim.healthPoints -= amount
+    }
+  }
+
+  heal(amount: number, victim: Character): void {
+    if(victim.isAlive) {
+      victim.healthPoints = (amount + victim.healthPoints > victim.maxHealthPoints) ? (victim.maxHealthPoints) : (victim.healthPoints + amount)
     }
   }
 }
