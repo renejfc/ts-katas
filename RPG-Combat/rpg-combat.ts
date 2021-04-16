c8be03b97d78d0f094aad7b6e8d5e205bf932feb
ReneJFC <hola@renejfc.dev>
2021-04-16 20:45:10 +0200

    :sparkles: instance id & can't attack itself

--- RPG-Combat/rpg-combat.ts
+++ RPG-Combat/rpg-combat.ts
@@ -1,4 +1,5 @@
 interface ICharacter {
+  id: number
   healthPoints: number
   maxHealthPoints: number
   level: number
@@ -8,23 +9,29 @@ interface ICharacter {
 }
 
 export class Character implements ICharacter {
+  static instanceCount: number = 0
+
+  id: number
   healthPoints: number
   maxHealthPoints: number = 1000
   level: number
   isAlive: boolean
 
   constructor({hp, level, isAlive}: {hp?: number, level?: number, isAlive?: boolean} = {}) {
+    this.id = ++Character.instanceCount
     this.healthPoints = (typeof hp !== 'undefined') ? hp : 1000
     this.level = level || 1
     this.isAlive = isAlive || (this.healthPoints > 0)
   }
 
   attack(amount: number, target: Character): void {
-    if(target.healthPoints < amount ) {
-      target.isAlive = false
-      target.healthPoints = 0
-    } else {
-      target.healthPoints -= amount
+    if (this.id != target.id) {
+      if(target.healthPoints < amount) {
+        target.isAlive = false
+        target.healthPoints = 0
+      } else {
+        target.healthPoints -= amount
+      }
     }
   }
 
